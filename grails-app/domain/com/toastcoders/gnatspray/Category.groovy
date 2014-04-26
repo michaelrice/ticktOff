711282ad241787e5b54ae040f2ffe77c98ddf2f5
Aaron Brown <brown.aaron.lloyd@gmail.com>
2014-04-26 01:33:35 -0500

    Removing ordering.
    Ordering should be done by using a List with a Has-Many in the Queue.

--- grails-app/domain/com/toastcoders/gnatspray/Category.groovy
+++ grails-app/domain/com/toastcoders/gnatspray/Category.groovy
@@ -3,11 +3,11 @@ package com.toastcoders.gnatspray
 class Category {
 
     String name
-    int position
     boolean showInList = true
 
     static belongsTo = [queue: Queue]
 
     static constraints = {
+        name blank: false, unique: 'queue', pattern: /[-\.a-zA-Z0-9 ]+/
     }
 }
